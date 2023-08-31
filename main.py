b22d8b20859795423164ddf1d69bce24f32e3db2
amna200 <amnaabdallah.2004@gmail.com>
2023-08-31 20:42:39 +0300

    TODO 2

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import uuid
 """ITF 08 Final Project Attendance System
 # TODO 1 Enter your name and submission date
 Name : Amna ELNaami
 Delivery Date : 8 31 23
 """
+
+# TODO 2 Define Course Class and define constructor with
+class Course:
+    def __init__(self, course_name, course_mark):
+        self.course_id = uuid.uuid4()
+        self.course_name = course_name
+        self.course_mark = course_mark
+
+course_name = input("Enter the course name: ")
+course_mark = input("Enter the course mark: ")
+
+course = Course(course_name, course_mark)
+print("Course ID:", course.course_id)
+print("Course Name:", course.course_name)
+print("Course Mark:", course.course_mark)
+

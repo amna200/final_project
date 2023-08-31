3309950d09c62ebf9c8c6376495c2b4d69d90fa6
amna200 <amnaabdallah.2004@gmail.com>
2023-08-31 21:19:28 +0300

    TODO 4

--- main.py
+++ main.py
@@ -23,3 +23,27 @@ print("Course Mark:", course.course_mark)
 class Student:
     # TODO 3 define static variable indicates total student count
     total_students = 0
+    # TODO 4 define a constructor which includes
+    # student_id (unique using uuid module)
+    # student_name (user input)
+    # student_age (user input)
+    # student_number (user_input)
+    # courses_list (List of Course Objects)
+    def __init__(self, student_name, student_age, student_number, courses_list):
+        self.student_id = uuid.uuid4()
+        self.student_name = student_name
+        self.student_age = student_age
+        self.student_number = student_number
+        self.courses_list = courses_list
+        Student.total_students += 1
+
+student_name = input("Enter student name: ")
+student_age = int(input("Enter student age: "))
+student_number = input("Enter student number: ")
+courses_list = []
+student = Student(student_name, student_age, student_number, courses_list)
+print("Student name "+student_name)
+print("Student age "+str(student_age))
+print("Student number "+student_number)
+print("Total students:", Student.total_students)
+
